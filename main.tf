45c0784e695b0d11ba78f01b270b04bfccb5e3f5
szabodaniel17 <46632395+szabodaniel17@users.noreply.github.com>
2023-03-28 14:09:36 +0300

    load balancer

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ module "vpc" {
     Environment = "dev"
   }
 }
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -38,6 +39,46 @@ resource "aws_instance" "blog" {
 
   tags = {
     Name = "HelloWorld"
+  }
+}
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
+
+  target_groups = [
+    {
+      name_prefix      = "blog"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "Dev"
   }
 }
 
